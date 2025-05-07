937bc489fd715f891981abd6c10612e48cee5d73
jov <git@jovylle.com>
2025-05-07 12:51:11 +0800

    add beautiful bg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="app"
-    class="min-h-screen bg-base-200 text-base-content flex flex-col transition-colors duration-300"
+    class="min-h-screen bg-base-200 bg-cross-lines text-base-content flex flex-col transition-colors duration-300"
   >
     <MainDrawer />
     <main class="py-6 sm:py-8 px-4 w-full max-w-screen-lg mx-auto text-center">
