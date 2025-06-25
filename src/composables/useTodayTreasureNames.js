b87530b661c83fb9f45c22a5a6b47eea32af5786
j <git@jovylle.com>
2025-06-25 19:36:58 +0800

    possible treasure progress

---
+++ src/composables/useTodayTreasureNames.js
@@ -0,0 +1,37 @@
+// composables/useTodayTreasureNames.js
+import { computed } from "vue";
+import { DIGGING_FORMATIONS } from '../../src_other/features/game/types/desert'
+import { useLandData } from "@/composables/useLandData";
+
+export function useTodayTreasureNames () {
+  const { landData } = useLandData();
+
+  return computed(() => {
+    const keys = landData.value?.state?.desert?.digging?.patterns || [];
+    const set = new Set();
+
+    console.log("useTodayTreasureNames → pattern keys:", keys);
+
+    for (const key of keys) {
+      const formation = DIGGING_FORMATIONS[key];
+      if (!formation) {
+        console.warn("❌ Pattern key not found:", key);
+        continue;
+      }
+
+      console.log(`🧩 Formation for ${key}:`, formation);
+
+      formation.forEach(({ name }) => {
+        if (name) {
+          set.add(name);
+        }
+      });
+    }
+    
+
+    const treasures = Array.from(set);
+    console.log("Today’s treasure names (final):", treasures);
+    return treasures;
+  });
+  
+}
